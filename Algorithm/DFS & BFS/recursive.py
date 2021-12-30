de4a71e1f4c82bf7af6f04ce2a455141288c2f65
bishoe01 <bishoe01@kakao.com>
2021-12-30 22:41:25 +0900

    add DFS&BFS

---
+++ Algorithm/DFS & BFS/recursive.py
@@ -0,0 +1,22 @@
+def dfs(graph,node_v,visited):
+    visited[node_v] = True
+    print(node_v, end=" ")
+    for i in graph[node_v]:
+        if not visited[i]:
+            dfs(graph,i,visited)
+
+graph = [  #인접한 노드들을 2차원 배열로 표현
+    [],  #인덱스 0 은 지워두기
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
+
+visited = [False]*9
+
+dfs(graph,1,visited)
